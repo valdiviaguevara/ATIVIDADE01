50742ad215f947690e5f69746896778a7b58fb68
valdiviaguevara <valdiviaguevara@gmail.com>
2018-03-30 21:56:53 -0300

    Update main.hs

--- Lista 1/Exercicio 01/main.hs
+++ Lista 1/Exercicio 01/main.hs
@@ -1,5 +1,5 @@
 --Exercicio 01
---Exercício 01: Execute as seguintes operações utilizando o menor número de parênteses:
+--Execute as seguintes operações utilizando o menor número de parênteses:
 --2⋅3+5
 --2+2⋅3+1
 --3^4+5⋅2^5+1
