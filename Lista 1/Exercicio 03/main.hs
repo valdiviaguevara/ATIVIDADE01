dff5d875a19bbab93b295bb0ca8b448bc368dba3
valdiviaguevara <valdiviaguevara@gmail.com>
2018-03-31 01:40:11 -0300

    Exercicio 03
    Exercicio 03

--- Lista 1/Exercicio 03/main.hs
+++ Lista 1/Exercicio 03/main.hs
@@ -1,6 +1,6 @@
 --Exercicio 03
 --Faça uma função mult5 x que retorne True caso a entrada seja múltiplo de 5 e False caso contrário.
-mult3 :: Int -> Bool 
-mult3 x = (x `rem` 5 == 0)
+mult5 :: Int -> Bool 
+mult5 x = (x `rem` 5 == 0)
 main = do
-    print (mult3 500)
+    print (mult5 500)
