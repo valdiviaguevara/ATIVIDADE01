40c5747eae1d0f8640b9cb15d82fcc77b156b688
valdiviaguevara <valdiviaguevara@gmail.com>
2018-03-31 18:55:43 -0300

    Exercicio 07
    Exercicio 07

--- Lista 1/Exercicio 07/main.hs
+++ Lista 1/Exercicio 07/main.hs
@@ -1,8 +1,7 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
+--Exercicio 07
+--Faca uma função que receba um ângulo a e retorne uma tupla contendo o seno da metade desse ângulo utilizando a identidade:
+senmedade :: Double -> (Double,Double)
+senmedade x = (sqrt((1-cos(x))/2),(-1)*sqrt((1-cos(x))/2))
 main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+  print (senmedade 90)
+  print (senmedade 95.6)
