49bf9899f1a723429b33a396de2e3b6a24b90381
valdiviaguevara <valdiviaguevara@gmail.com>
2018-03-30 22:24:03 -0300

    Exercicio 01
    Exercicio 01

--- Lista 1/Exercicio 01/main.hs
+++ Lista 1/Exercicio 01/main.hs
@@ -1,10 +1,10 @@
 --Exercicio 01
 --Execute as seguintes operações utilizando o menor número de parênteses:
---2⋅3+5
---2+2⋅3+1
---3^4+5⋅2^5+1
+---2⋅3+5
+---2+2⋅3+1
+---3^4+5⋅2^5+1
 module Main where
--- |Fun ̧c~ao principal
+-- |Funcao principal
 main :: IO ()
 main = do
     print (2*3+5)
