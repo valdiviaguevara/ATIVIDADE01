91c40a245f553edf609fb765acf1028df3b2dedc
valdiviaguevara <valdiviaguevara@gmail.com>
2018-03-30 21:56:20 -0300

    Exercicio 01
    Program

--- Lista 1/Exercicio 01/main.hs
+++ Lista 1/Exercicio 01/main.hs
@@ -1,4 +1,8 @@
 --Exercicio 01
+--Exercício 01: Execute as seguintes operações utilizando o menor número de parênteses:
+--2⋅3+5
+--2+2⋅3+1
+--3^4+5⋅2^5+1
 module Main where
 -- |Fun ̧c~ao principal
 main :: IO ()
