717885c230b62dfe4d146ab34612b02e4c8d4f6e
valdiviaguevara <valdiviaguevara@gmail.com>
2018-03-31 21:07:33 -0300

    Exercicio 10
    Exercicio 10

--- Lista 1/Exercicio 10/main.hs
+++ Lista 1/Exercicio 10/main.hs
@@ -1,8 +1,9 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
+--Exercicio 10
+-- Crie uma tupla em que o primeiro elemento tem metade dos anos bissextos e o segundo elemento a outra metade.
+bissexto :: Integer -> Bool
+bissexto ano = (ano `rem` 4 == 0)
 main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+  let ano_atual=2018
+  let listaBissextos=[x|x<-[1..ano_atual],(bissexto x==True)]
+  let tupla_mitade = [(listaBissextos !! i,listaBissextos !! j) | i <- [0.. ((length listaBissextos)`div`2)-1], j <- [((length listaBissextos)`div`2).. (length listaBissextos)-1]]
+  print (tupla_mitade)
