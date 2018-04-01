053c0711d0c49714eeb85e16ab49b3c3e4601077
valdiviaguevara <valdiviaguevara@gmail.com>
2018-04-01 04:25:49 -0300

    Exercicio 12
    Exercicio 12

--- Lista 1/Exercicio 12/main.hs
+++ Lista 1/Exercicio 12/main.hs
@@ -1,8 +1,8 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
+--Exercicio 12
+-- Dada a string “0123456789”, crie uma lista com os dígitos em formato Integer.
+
 main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+    let str = "0123456789"
+    let tamanho_string=length str
+    let lista_Substring_Enteiro=map (\ x -> read[str!!x]::Int) [0..tamanho_string-1]
+    print lista_Substring_Enteiro
