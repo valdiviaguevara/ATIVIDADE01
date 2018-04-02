83d5d45950ed5ce6fcbbd2bcd30850c20e020bd2
valdiviaguevara <valdiviaguevara@gmail.com>
2018-04-02 06:12:04 -0300

    Exercicio 04
    Exercicio 04

--- Lista 2/Exercicio 04/main.hs
+++ Lista 2/Exercicio 04/main.hs
@@ -1,8 +1,15 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
+--Exercicio 04
+--Faça uma função que determine se um número é primo.
+elimina_element_divisivel :: Integer -> [Integer] -> [Integer]
+elimina_element_divisivel numero list_num = [ x | x <- list_num, x `mod` numero /= 0 ]
+list_num_primos :: [Integer] -> [Integer]
+list_num_primos []     = []
+list_num_primos (n:lista_p) = n : list_num_primos (elimina_element_divisivel n lista_p)
+lista_primos :: [Integer]
+lista_primos = list_num_primos [2..]
+e_Primo :: Integer -> Bool
+e_Primo n = head (dropWhile (<n) lista_primos) == n
 main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+  print (e_Primo 2) 
+  print (e_Primo 12) 
+  print (e_Primo 19) 
