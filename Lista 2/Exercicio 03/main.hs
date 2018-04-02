187357435d874bcdff3196c9e17b01625356a7fb
valdiviaguevara <valdiviaguevara@gmail.com>
2018-04-02 05:35:46 -0300

    Exercicio 03
    Exercicio 03

--- Lista 2/Exercicio 03/main.hs
+++ Lista 2/Exercicio 03/main.hs
@@ -1,8 +1,14 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
+--Exercicio 03
+--Implemente uma função que faz a multiplicação etíope entre dois números
+multiplicacao_etiope :: Integer->Integer->Integer
+multiplicacao_etiope 1 y= y
+-- multiplicacao_etiope
+multiplicacao_etiope x y=multiplicacao_etiope' x y 0
+  where
+    multiplicacao_etiope' 1 y r
+      |(y `rem` 2 /=0 )=multiplicacao_etiope' 1 y (r+y)
+      |otherwise  = y
+    multiplicacao_etiope' x y r = multiplicacao_etiope' (x `div` 2) (y * 2) (r) 
 main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+  print (multiplicacao_etiope 4 3) 
+  
