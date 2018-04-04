deb273e1a1e2dbe7ecaf5b754a87920a4c09b633
valdiviaguevara <valdiviaguevara@gmail.com>
2018-04-04 07:53:36 -0300

    Exercicio 06
    Exercicio 06

--- Lista 2/Exercicio 06/main.hs
+++ Lista 2/Exercicio 06/main.hs
@@ -1,8 +1,16 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
+--Exercicio 06
+--Faça uma função que calcule a persistência aditiva de um número.
+contar_Digitos :: Integer -> Int
+contar_Digitos = go 1 . abs
+    where
+        go ds n = if n >= 10 then go (ds + 1) (n `div` 10) else ds
+soma_digitos :: Integer -> Integer
+soma_digitos numero = sum [read [digito] | digito <- show numero]
+soma_Persistencia :: Integer->Integer
+soma_Persistencia s_digit_nova = soma_Persistencia' s_digit_nova 0 0
+  where 
+    soma_Persistencia' s_digit_nova 1 r= r
+    soma_Persistencia' s_digit_nova contador r=soma_Persistencia' (soma_digitos s_digit_nova) (contar_Digitos (soma_digitos s_digit_nova)) (r+1)
+
 main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+  print (soma_Persistencia  199)
