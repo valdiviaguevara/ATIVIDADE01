db71fa42fd48681c81c8f36c6e27691396f57f04
valdiviaguevara <valdiviaguevara@gmail.com>
2018-04-04 18:46:23 -0300

    Exercicio 08
    Exercicio 08

--- Lista 2/Exercicio 08/main.hs
+++ Lista 2/Exercicio 08/main.hs
@@ -1,8 +1,10 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
+--Exercicio 08
+--Faça uma função que calcule o elemento (i,j) do triângulo de pascal.
+fator = product . enumFromTo 1
+coeficiente_binomial n k = (fator n) `div` ((fator k) * (fator $ n - k))
+triangulo_pascal n = map (coeficiente_binomial $ n - 1) [0..n-1]
+--Encontrar o elemento del array
+elemento_Triangulo_Pascal :: Integer->Int->Integer
+elemento_Triangulo_Pascal i j =(triangulo_pascal i) !! j 
 main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+  print (elemento_Triangulo_Pascal  6 2)
