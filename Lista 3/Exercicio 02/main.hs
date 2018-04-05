987253bba95d5d583a945f8a1cd5f82db1d36d3c
valdiviaguevara <valdiviaguevara@gmail.com>
2018-04-04 22:31:43 -0300

    Exercicio 02
    Exercicio 02

--- Lista 3/Exercicio 02/main.hs
+++ Lista 3/Exercicio 02/main.hs
@@ -1,8 +1,13 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
+--Exercicio 02
+--Crie uma função projectEuler5 que retorna o primeiro número natural que retorna True para a função do exercício anterior. Pense em como reduzir o custo computacional.
+list_divisivel20::Integer->[Integer]
+list_divisivel20 i= [n|n<-[1..20],i `mod`n==0]
+divisivel20::Integer->Bool
+divisivel20 x =length (list_divisivel20 x) ==20 
+projectEuler5 :: Integer->Integer
+projectEuler5 valor =head([num|num<-[1..valor],divisivel20 num ==True])
 main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+  --Mostra true de todos os numeros de 1 a 20 que sao divisiveispor este numero
+  print (divisivel20 232792560)
+  --Mostra o primer Elemento da lista que cumpre o anterior funcao
+  print (projectEuler5 232792560)
