81e8a2a7c0e529f0cdc5e39b0ec5ead8b9373b84
valdiviaguevara <valdiviaguevara@gmail.com>
2018-04-10 02:42:08 -0300

    Exercicio 03
    Exercicio 03

--- Lista 4/Exercicio 03/main.hs
+++ Lista 4/Exercicio 03/main.hs
@@ -1,8 +1,2 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
-main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+--Exercicio 03
+--Faça uma função que calcule a soma da diagonal secundária de uma matriz.
