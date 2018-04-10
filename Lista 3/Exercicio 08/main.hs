5cd029a6048aedf95cf98ff583d96dd08146ad25
valdiviaguevara <valdiviaguevara@gmail.com>
2018-04-10 02:14:00 -0300

    Exercicio 08
    Exercicio 08

--- Lista 3/Exercicio 08/main.hs
+++ Lista 3/Exercicio 08/main.hs
@@ -1,8 +1,15 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
+--Exercicio 08
+--Encontre o número x entre 1 e 1.000.000 que tem a maior sequência de Collatz. (Project Euler 14)
+import Data.List (maximumBy)
+import Data.Ord(comparing)
+collatz :: Integer->Integer
+collatz x 
+    | even x = div x 2
+    | otherwise = 3*x + 1
+collatzLen :: Integer -> Int
+collatzLen x = length $ takeWhile (>1) $ x: prox x
+    where prox x = collatz x : (prox (collatz x))
+project_euler_14 :: Integer
+project_euler_14=maximumBy (comparing collatzLen) [1..1000000]
 main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+  print (project_euler_14)
