6d0cb6eb01920b0b8c5f65394d369de6492f8d73
valdiviaguevara <valdiviaguevara@gmail.com>
2018-04-10 02:39:48 -0300

    Exercicio 02
    Exercicio 02

--- Lista 4/Exercicio 02/main.hs
+++ Lista 4/Exercicio 02/main.hs
@@ -1,8 +1,12 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
+--Exercicio 02
+--Faça uma função que calcule a soma da diagonal principal de uma matriz.
+soma_diagonal_principal :: Num a => [[a]]-> a
+soma_diagonal_principal d = sum [d !! i !! i | i <- [0..size] ]
+    where size = length d - 1
 main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+  -- 10  15  16  19 
+  --  8   4   6   2 
+  --  1   5   4   4 
+  --  1   1   5   2 
+  --Suma_Diagonal = 10 + 4 + 4 + 2 = 20
+  print (soma_diagonal_principal [[10,15,16,19],[8,4,6,2],[1,5,4,4],[1,1,5,2]])
