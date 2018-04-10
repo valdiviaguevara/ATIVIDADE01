3cfc643e561bcb5dd979f34a886c65d69e9018b7
valdiviaguevara <valdiviaguevara@gmail.com>
2018-04-10 01:53:21 -0300

    Exercicio 04
    Exercicio 04

--- Lista 3/Exercicio 04/main.hs
+++ Lista 3/Exercicio 04/main.hs
@@ -1,8 +1,11 @@
---Exercicio 01
-module Main where
--- |Fun ̧c~ao principal
-main :: IO ()
+--Exercicio 04
+--Utilizando a lista anterior, calcule a soma dos números de Fibonacci pares dos valores que não excedem 4.000.000. (Project Euler 2)
+list_num_fibonacci :: [Integer]
+list_num_fibonacci = 1 : 1 : proximo 1 1
+    where proximo a b = a + b : proximo b (a+b)
+project_Euler_2 :: Integer
+project_Euler_2 = sum $ filter even $ takeWhile (<4000000) $ list_num_fibonacci
 main = do
-    print (2*3+5)
-    print (2+2*3+1)
-    print (3^4+5*2^5+1)
+  -- print (take 4000000 list_num_fibonacci)
+  print (project_Euler_2)
+  
