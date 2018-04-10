f0f5c8ddf11dc81824fb804d81c48a09103747b9
valdiviaguevara <valdiviaguevara@gmail.com>
2018-04-10 01:40:54 -0300

    Exercicio 05
    Exercicio 05

--- Lista 3/Exercicio 05/main.hs
+++ Lista 3/Exercicio 05/main.hs
@@ -1,6 +1,6 @@
 --Exercicio 05
 --Faça uma função para calcular o produto escalar entre dois vetores.
-dotProduct :: [Integer] -> [Integer] -> Integer
-dotProduct x y = sum ( zipWith (*) x y )
+produto_escalar :: [Integer] -> [Integer] -> Integer
+produto_escalar x y = sum ( zipWith (*) x y )
 main = do
-  print (dotProduct [1,2,3,4] [5,6,7,8])
+  print (produto_escalar [1,2,3,4] [5,6,7,8])
